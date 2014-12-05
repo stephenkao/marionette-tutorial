8f73cfb1a3c1f9da6e5b6b7ef5384921baffc029
Stephen Kao <stephen.kao@gawker.com>
2014-12-05 16:45:51 -0500

    Adding some crazy SCSS stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,48 @@
-/*global module */
+/*global module, require */
 
 /**
  * Marionette tutorial Gruntfile
  * DON'T TOUCH THIS, YOU DAMN, DIRTY APES!
  */
-(function () {
+(function() {
 	'use strict';
 
 	module.exports = function (grunt) {
 		grunt.initConfig({
-			pkg: grunt.file.readJson('package.json'),
+			pkg: grunt.file.readJSON('package.json'),
+
+			////////// JavaScript
+			jshint: {
+				options: {
+					jshintrc: '.jshintrc',
+					reporter: require('jshint-stylish'),
+					reporterOutput: 'output/contrib-jshint.xml',
+					force: true
+				},
+				dev: {
+					src: [
+						'javascripts',
+						'!javascripts/lib'
+					]
+				}
+			},
 
 			////////// CSS
+			scsslint: {
+				options: {
+					bundleExec: false,
+					config: '.scss-lint.yml',
+					reporterOutput: 'output/scss-lint-report.xml',
+					colorizeOutput: true,
+					maxBuffer: Infinity,
+					force: true
+				},
+				dev: {
+					src: [
+						'scss'
+					]
+				}
+			},
 			compass: {
 				dev: {
 					options: {
@@ -22,9 +53,27 @@
 			},
 
 			watch: {
+				options: {
+					spawn: false,
+					interval: 5007
+				},
+
+				js: {
+					files: [
+						'javascripts/**/*.js',
+						'!javascripts/lib/**/*.js'
+					],
+					tasks: [
+						'jshint:dev'
+					]
+				},
 				sass: {
-					files: ['scss/**/*.scss'],
-					tasks: ['compass:dev']
+					files: [
+						'scss/**/*.scss'
+					],
+					tasks: [
+						'compass:dev'
+					]
 				}
 			}
 		});
@@ -32,7 +81,13 @@
 		grunt.loadNpmTasks('grunt-contrib-compass');
 		grunt.loadNpmTasks('grunt-contrib-watch');
 
-		grunt.registerTask('compile', ['compass']);
-		grunt.registerTask('default', 'watch');
+		grunt.registerTask('default', [
+			'compass:dev',
+			'jshint:dev'
+		]);
+		grunt.registerTask('run', [
+			'watch:sass',
+			'watch:js'
+		]);
 	};
-});
+})();
