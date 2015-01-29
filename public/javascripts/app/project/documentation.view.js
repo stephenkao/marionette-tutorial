25a5c80656efd864c1d02d0937cf4810ddd56ee2
Stephen Kao <stephen.kao@gawker.com>
2015-01-29 13:08:24 -0500

    Updating documentation view -- populated with actual project data

--- public/javascripts/app/project/documentation.view.js
+++ public/javascripts/app/project/documentation.view.js
@@ -6,18 +6,31 @@
 define([
 	// Libraries
 	'backbone.marionette',
+	'lib/xdate',
 	// Templates
 	'templates/lesir/components/project/documentation'
 ], function (
 	// Libraries
-	Marionette
+	Marionette,
+	XDate
 ) {
 	'use strict';
 
 	var DocumentationView = Marionette.ItemView.extend({
 
 		////////// Initialization
-		template: lesir.components.project.documentation
+		template: lesir.components.project.documentation,
+
+		////////// Rendering
+		serializeData: function () {
+			var project = this.model.attributes,
+				lastUpdate = project.updates.sortBy('created').reverse()[0];
+
+			return {
+				projectTitle: project.title,
+				lastUpdateTime: new XDate(lastUpdate.get('created')).toString('MM / dd / yy')
+			};
+		}
 	});
 
 	return DocumentationView;
